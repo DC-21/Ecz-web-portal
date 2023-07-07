4724566e2e22de98630578074e944f0bb41fa3ca
DC-21 <cholahkuboko@gmail.com>
2023-07-07 14:57:08 +0200

    first commit

--- src/Pages/News.jsx
+++ src/Pages/News.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
-
+import React from "react";
 
 const News = () => {
   return (
-    <div className='px-4 w-full'>
-        <div className="w-full px-8 mt-1 mb-4 rounded h-screen flex bg-[#c4dfc4] justify-center items-center"></div>
+    <div className="px-4 w-full">
+      <div className="w-full px-8 mt-1 mb-4 rounded h-screen flex flex-col bg-[#c4dfc4] justify-center items-center">
+        <h1 className="text-xl underline">Latest ECZ News</h1>
+        <h2>
+          ECZ has nullified results for over 9,000 candidates from the 2022 exam
+        </h2>
+        <p>
+          The Examination Council of Zambia -ECZ- has nullified results for over
+          9,000 candidates from the 2022 examinations, countrywide for various
+          malpractices.
+        </p>
+        <p>
+          Eleven centres have since been suspended for two years while 24
+          teachers who were involved in the malpractice have been reported to
+          the Ministry of Education for disciplinary action. ECZ Executive
+          Director Micheal Chilala said this at a media briefing in Lusaka
+          yesterday.
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default News
+export default News;
