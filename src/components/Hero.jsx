542a8d506d55c815bbed69cb76c30fa1573511bf
DC-21 <cholahkuboko@gmail.com>
2023-07-07 10:21:48 +0200

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Hero = () => {
   return (
     <div className="px-4 w-full">
-      <div className="w-full px-8 mt-6 rounded h-screen flex bg-[#c4dfc4] justify-center items-center">
+      <div className="w-full px-8 mt-1 rounded h-screen flex bg-[#c4dfc4] justify-center items-center">
         <div>
           <h1>Welcome to the Examinations Council Of Zambia.</h1>
         </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="px-4 py-3 w-full sticky top-4">
+    <div className="px-4 py-3 w-full sticky top-0">
       <div className="w-full rounded flex py-6 px-8 bg-[#c4dfc4] justify-between shadow shadow-black">
         <div className="flex">
           <a>ECZ</a>
