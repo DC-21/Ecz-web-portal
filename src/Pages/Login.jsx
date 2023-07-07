db01bf1ba244112f72308e70405a5ef01db69c5b
DC-21 <cholahkuboko@gmail.com>
2023-07-07 19:53:32 +0200

    first commit

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Login = () => {
   return (
     <div className="flex flex-col h-screen justify-center items-center">
-                <h1 className="text-xl text-center pb-4">Login</h1>
+                <h1 className="text-xl text-center pb-4">Login Please</h1>
       <div className="border border-green-400 py-14 px-10 rounded">
         <div className="text-center pb-10 pt-4">
           <label htmlFor="username">Username</label>
@@ -17,11 +17,14 @@ const Login = () => {
         <div className="text-center pt-0">
           <label htmlFor="username">Password</label>
           <input
-            type="text"
-            id="username"
+            type="password"
+            id="password"
             className="block w-64 px-4 py-2 mt-2 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500"
             placeholder="Password"
           />
+        </div>
+        <div className="text-center pt-6">
+            <button className="px-10 py-3 bg-[#00a536] rounded mt-8">Login</button>
         </div>
       </div>
     </div>
