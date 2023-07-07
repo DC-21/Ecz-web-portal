044f5c87f153129ef98daf2b67221c622c7c9a0b
DC-21 <cholahkuboko@gmail.com>
2023-07-07 19:59:15 +0200

    first commit

---
+++ src/Pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='w-full pt-6'>
+        <div className='w-full h-screen bg-[#00a536] px-8'></div>
+    </div>
+  )
+}
+
+export default About
