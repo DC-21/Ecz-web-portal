61a32c314113fab884f100fc39ab51cc70d31fe4
DC-21 <cholahkuboko@gmail.com>
2023-07-07 15:15:07 +0200

    first commit

--- src/Pages/News.jsx
+++ src/Pages/News.jsx
@@ -4,22 +4,28 @@ const News = () => {
   return (
     <div className="px-4 w-full">
       <div className="w-full px-8 mt-1 mb-4 rounded h-screen flex flex-col bg-[#c4dfc4] justify-center items-center">
-        <h1 className="text-xl underline">Latest ECZ News</h1>
-        <h2>
-          ECZ has nullified results for over 9,000 candidates from the 2022 exam
+        <h1 className="text-3xl underline text-black">Latest ECZ News</h1>
+        <h2 className="pt-8 text-2xl">
+          More female candidates have registered for 2023 exams compared to
+          males – ECZ
         </h2>
-        <p>
-          The Examination Council of Zambia -ECZ- has nullified results for over
-          9,000 candidates from the 2022 examinations, countrywide for various
-          malpractices.
-        </p>
-        <p>
-          Eleven centres have since been suspended for two years while 24
-          teachers who were involved in the malpractice have been reported to
-          the Ministry of Education for disciplinary action. ECZ Executive
-          Director Micheal Chilala said this at a media briefing in Lusaka
-          yesterday.
-        </p>
+        <div>
+          <p>
+            THE Examinations Council of Zambia (ECZ) has disclosed that more
+            female candidates have registered for the 2023 examinations compared
+            to male candidates. And Chilala says 24 names of teachers who were
+            suspected to have been involved in examination malpractice during
+            the 2022 GCE and Grade 9 External examinations have been submitted
+            to the Ministry of Education for the disciplinary process. Speaking
+            during a media briefing, Wednesday, ECZ Executive Director Dr
+            Michael Chilala said over 500,000 females registered for the
+            examinations compared to the 471,790 male candidates. “There are
+            significantly more female candidates for all the 2023 examinations
+            than male candidates. A total of 260, 367 Grade 7; 165,116 Grade 9;
+            and 84,085 Grade 12 female candidates registered for the
+            examinations…....{" "}
+          </p>
+        </div>
       </div>
     </div>
   );
