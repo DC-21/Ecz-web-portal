3d068f64cde334a792434fff5c02464b201b25c9
DC-21 <cholahkuboko@gmail.com>
2023-07-07 10:37:29 +0200

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import eczhero from '../images/eczhero.png'
+import eczhero from "../images/eczhero.png";
 
 const Hero = () => {
   return (
@@ -7,8 +7,18 @@ const Hero = () => {
       <div className="w-full px-8 mt-1 rounded h-screen flex bg-[#c4dfc4] justify-center items-center">
         <div className="text-center">
           <h1>Welcome to the Examinations Council Of Zambia.</h1>
-          <img src={eczhero} alt="ecz" className="pt-8 pb-8 rounded justify-center" />
-          <h1>Log in with your shool credentials in order to check out the results</h1>
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <img
+              src={eczhero}
+              alt="ecz"
+              className="pt-8 pb-8"
+              style={{ width: "50%", height: "auto", borderRadius: "10%" }}
+            />
+          </div>
+
+          <h1>
+            Log in with your shool credentials in order to check out the results
+          </h1>
         </div>
       </div>
     </div>
