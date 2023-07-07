a94e734880d85fe0e90d7ebb3b22588e878ab541
DC-21 <cholahkuboko@gmail.com>
2023-07-07 10:45:54 +0200

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,24 +1,27 @@
 import React from "react";
-import eczhero from "../images/hero.png";
+import hero from "../images/hero.png";
 
 const Hero = () => {
   return (
     <div className="px-4 w-full">
       <div className="w-full px-8 mt-1 rounded h-screen flex bg-[#c4dfc4] justify-center items-center">
         <div className="text-center">
-          <h1>Welcome to the Examinations Council Of Zambia.</h1>
+          <h1 className="text-xl">Welcome to the Examinations Council Of Zambia.</h1>
           <div style={{ display: "flex", justifyContent: "center"}}>
             <img
-              src={eczhero}
+              src={hero}
               alt="ecz"
               className="pt-8 pb-8"
               style={{ width: "50%", height: "auto", borderRadius: "10%" }}
             />
           </div>
 
-          <h1>
-            Log in with your shool credentials in order to check out the results
+          <h1 className="text-xl">
+            Log in with your school credentials in order to check out the results
           </h1>
+          <div className="pt-5">
+            <button className="px-3 py-2 bg-[#00a758] text-white rounded">Log In</button>
+          </div>
         </div>
       </div>
     </div>
