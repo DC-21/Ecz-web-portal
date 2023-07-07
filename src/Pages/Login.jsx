0e10ac700270de1f95bcff014a741f3ddf2096cf
DC-21 <cholahkuboko@gmail.com>
2023-07-07 18:26:19 +0200

    first commit

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -3,26 +3,27 @@ import React from "react";
 const Login = () => {
   return (
     <div className="flex flex-col h-screen justify-center items-center">
-        <div>
-        div className="text-center pb-6 border border-slate-400">
-        <label htmlFor="username">Username</label>
-        <input
-          type="text"
-          id="username"
-          className="block w-64 px-4 py-2 mt-2 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500"
-          placeholder="Username"
-        />
-      </div>
-      <div className="text-center pt-4">
-        <label htmlFor="username">Password</label>
-        <input
-          type="text"
-          id="username"
-          className="block w-64 px-4 py-2 mt-2 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500"
-          placeholder="Password"
-        />
-      </div>
+      <div className="border border-green-400 py-14 px-10 rounded">
+        <h1 className="text-center pb-4">Login</h1>
+        <div className="text-center pb-10 pt-4">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            className="block w-64 px-4 py-2 mt-2 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+            placeholder="Username"
+          />
         </div>
+        <div className="text-center pt-0">
+          <label htmlFor="username">Password</label>
+          <input
+            type="text"
+            id="username"
+            className="block w-64 px-4 py-2 mt-2 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+            placeholder="Password"
+          />
+        </div>
+      </div>
     </div>
   );
 };
