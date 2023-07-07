d7ecdd8da23b8c2622a64cc4585258be0611cfbe
DC-21 <cholahkuboko@gmail.com>
2023-07-07 14:43:44 +0200

    first commit

--- src/Pages/News.jsx
+++ src/Pages/News.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
+
 
 const News = () => {
   return (
-    <div>
-      <Navbar/>
-      <Footer/>
+    <div className='px-4 w-full'>
+        <div className="w-full px-8 mt-1 mb-4 rounded h-screen flex bg-[#c4dfc4] justify-center items-center"></div>
     </div>
   )
 }
