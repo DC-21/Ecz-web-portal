19bf847b56b09c2d0b5d998145cf19c6c4ccc623
DC-21 <cholahkuboko@gmail.com>
2023-07-07 11:53:21 +0200

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,19 +8,22 @@ const Navbar = () => {
           <a>ECZ</a>
         </div>
         <div className="flex gap-8 items-center cursor-pointer">
-          <a>Home</a>
-          <a>
+          <a className="hover:px-1 py-2 hover:bg-[#77aa77] text-white rounded inline-block w-14">
+            Home
+          </a>
+          <a className="inline-block">
             <select
               id="school-select"
-              className="block w-full px-4 py-2 bg-[#00a536] appearance-none cursor-pointer"
+              className="block w-24 h-10 px-4 py-2 bg-[#00a536] appearance-none cursor-pointer hover:bg-[#77aa77] text-white rounded"
             >
               <option value="">Schools</option>
               <option value="school2">Primary</option>
               <option value="school3">Secondary</option>
               <option value="school4">College</option>
-              <option value="school4">University</option>
+              <option value="school5">University</option>
             </select>
           </a>
+
           <a>News</a>
           <a>About</a>
         </div>
