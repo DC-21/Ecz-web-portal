454f2f329541d226a2dd8e9ff448d6177cf9fee2
DC-21 <cholahkuboko@gmail.com>
2023-07-07 10:14:25 +0200

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,21 +2,19 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className='px-4 py-3 w-full'>
-            <div className="w-full rounded flex py-6 px-8 bg-[#c4dfc4] justify-between">
-      <div className="flex">
-        <a>ECZ</a>
+    <div className="px-4 py-3 w-full sticky top-4">
+      <div className="w-full rounded flex py-6 px-8 bg-[#c4dfc4] justify-between shadow shadow-black">
+        <div className="flex">
+          <a>ECZ</a>
+        </div>
+        <div className="flex gap-8">
+          <a>Home</a>
+          <a>Schools</a>
+          <a>News</a>
+          <a>About</a>
+        </div>
+        <div>Log In</div>
       </div>
-      <div className="flex gap-8">
-        <a>Home</a>
-        <a>Schools</a>
-        <a>News</a>
-        <a>About</a>
-      </div>
-      <div>
-        Log In
-      </div>
-    </div>
     </div>
   );
 };
