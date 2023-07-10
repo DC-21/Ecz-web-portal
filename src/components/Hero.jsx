7f9650a5a023dd08c6dc81344eff98f649390b06
DC-21 <cholahkuboko@gmail.com>
2023-07-10 12:29:24 +0200

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import hero from "../images/hero.png";
 
 const Hero = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const handleLogout = () => {
+    // Perform logout logic here
+    setLoggedIn(false);
+  };
+
   return (
     <div className="px-4 w-full">
       <div className="w-full px-8 mt-1 mb-4 rounded h-screen flex bg-[#c4dfc4] justify-center items-center">
@@ -17,14 +24,34 @@ const Hero = () => {
             />
           </div>
 
-          <h1 className="text-xl">
-            Log in with your school credentials in order to check out the results
-          </h1>
-          <div className="pt-5">
-            <Link
-            to="/login"
-            className="px-3 py-2 bg-[#00a758] text-white rounded hover:bg-[#00a536]">Log In</Link>
-          </div>
+          {loggedIn ? (
+            <>
+              <h1 className="text-xl">You are logged in!</h1>
+              <div className="pt-5">
+                <button
+                  className="px-3 py-2 bg-[#00a758] text-white rounded hover:bg-[#00a536]"
+                  onClick={handleLogout}
+                >
+                  Log Out
+                </button>
+              </div>
+            </>
+          ) : (
+            <>
+              <h1 className="text-xl">
+                Log in with your school credentials in order to check out the results
+              </h1>
+              <div className="pt-5">
+                <Link
+                  to="/login"
+                  className="px-3 py-2 bg-[#00a758] text-white rounded hover:bg-[#00a536]"
+                >
+                  Log In
+                </Link>
+              </div>
+            </>
+          )}
+          {!loggedIn && <div style={{ height: "50px" }} />}
         </div>
       </div>
     </div>
