32c2b3bbb45c6e96604990946496436b394a5607
DC-21 <cholahkuboko@gmail.com>
2023-07-10 12:07:13 +0200

    first commit

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,17 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from 'axios';
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [loginMessage, setLoginMessage] = useState('');
 
   const handleLogin = async () => {
     try {
       const response = await axios.post('http://localhost:3000/login', { email, password });
       // Handle successful login
       console.log(response.data);
+      setLoginMessage('Successful login!');
     } catch (error) {
       console.error(error);
+      setLoginMessage('Login failed. Please try again.');
     }
   };
 
@@ -23,10 +26,27 @@ const Login = () => {
     setPassword(event.target.value);
   };
 
+  useEffect(() => {
+    let timeoutId;
+
+    if (loginMessage) {
+      timeoutId = setTimeout(() => {
+        setLoginMessage('');
+      }, 5000);
+    }
+
+    return () => clearTimeout(timeoutId);
+  }, [loginMessage]);
+
   return (
     <div className="flex flex-col h-screen justify-center items-center">
       <h1 className="text-xl text-center pb-4">Login Please</h1>
       <div className="border border-green-400 py-14 px-10 rounded">
+        {loginMessage && (
+          <div className="text-center pt-4">
+            <p>{loginMessage}</p>
+          </div>
+        )}
         <div className="text-center pb-10 pt-4">
           <label htmlFor="email">Email</label>
           <input
