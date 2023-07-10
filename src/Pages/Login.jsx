cc0b89da16f5d72b0ce1b7d949eff5397a641958
DC-21 <cholahkuboko@gmail.com>
2023-07-10 18:26:31 +0200

    first commit

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,22 +1,32 @@
-import React, { useState } from "react";
-import axios from 'axios';
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router";
 
 const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [loginMessage, setLoginMessage] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loginMessage, setLoginMessage] = useState("");
+  const navigation = useNavigate();
+  const [loading, setLoading] = useState(false);
 
   const handleLogin = async () => {
     try {
-      const response = await axios.post('http://localhost:3000/login', { email, password });
-      // Handle successful login
-      console.log(response.data);
-      setLoginMessage('Successful login!');
-      // Redirect to home page
-      window.location.href = '/';
+      setLoading(true);
+      if (email === "" || password === "") {
+        return setLoginMessage("Please fill in the form");
+      }
+      await axios
+        .post("http://localhost:3000/login", { email, password })
+        .then(() => {
+          setLoading(false);
+          setLoginMessage("Successful login!");
+          // Redirect to home page
+          navigation("/");
+        });
     } catch (error) {
       console.error(error);
-      setLoginMessage('Login failed. Please try again.');
+      setLoginMessage(error.response.data.message);
+      setLoading(false)
     }
   };
 
@@ -32,11 +42,6 @@ const Login = () => {
     <div className="flex flex-col h-screen justify-center items-center">
       <h1 className="text-xl text-center pb-4">Login Please</h1>
       <div className="border border-green-400 py-14 px-10 rounded">
-        {loginMessage && (
-          <div className="text-center pt-4">
-            <p>{loginMessage}</p>
-          </div>
-        )}
         <div className="text-center pb-10 pt-4">
           <label htmlFor="email">Email</label>
           <input
@@ -59,10 +64,22 @@ const Login = () => {
             onChange={handlePasswordChange}
           />
         </div>
+        {loginMessage && (
+          <div className="text-center pt-4 text-red-500">
+            <p>{loginMessage}</p>
+          </div>
+        )}
         <div className="text-center pt-6">
-          <button className="px-10 py-3 bg-[#00a536] rounded mt-8" onClick={handleLogin}>
-            Login
-          </button>
+          {loading ? (
+            <p> loading...</p>
+          ) : (
+            <button
+              className="px-10 py-3 bg-[#00a536] rounded mt-8"
+              onClick={handleLogin}
+            >
+              Login
+            </button>
+          )}
         </div>
       </div>
     </div>
